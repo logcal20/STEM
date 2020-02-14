b8fcec1d29c501f5fb6c94ae139871fe42861599
logcal20 <60793295+logcal20@users.noreply.github.com>
2020-02-14 08:24:33 -1000

    Create sqlCONNECT.php

---
+++ sqlCONNECT.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "user";
+$password = "password";
+$dbname = "school";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// check connection
+if($conn->connect_error){
+    die("connection failed:".$conn->connect_error);
+}
+
+$sql = "SELECT*FROM school";
+$result =$conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row['name']. " is " . $row['age']. " and in grade Level: " . $row['gradeLevel']. "<br><br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
